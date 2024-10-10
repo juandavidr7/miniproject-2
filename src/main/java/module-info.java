76d0eb18e5b2d9d5e3a2f78cd7f726d986b4fc5b
Juan Rincón <juandavidrinconl@gmail.com>
2024-10-10 16:45:19 -0500

    First implemented

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.miniproject2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.miniproject2 to javafx.fxml;
+    exports com.example.miniproject2;
+}
