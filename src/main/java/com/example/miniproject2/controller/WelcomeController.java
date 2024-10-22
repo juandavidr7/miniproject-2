eb36366a21ee87531a7e49684ebdca21690f1ef5
Juan Rincón <juandavidrinconl@gmail.com>
2024-10-22 09:52:05 -0500

    Progress in design of welcome-view

--- src/main/java/com/example/miniproject2/controller/WelcomeController.java
+++ src/main/java/com/example/miniproject2/controller/WelcomeController.java
@@ -1,4 +1,20 @@
 package com.example.miniproject2.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class WelcomeController {
+
+    @FXML
+    private ImageView titleImage;
+
+    // Método que se ejecuta después de cargar la interfaz
+    @FXML
+    public void initialize() {
+
+        Image image = new Image(getClass().getResourceAsStream("/com/example/miniproject2/images/title.png"));
+
+        titleImage.setImage(image);
+    }
 }
